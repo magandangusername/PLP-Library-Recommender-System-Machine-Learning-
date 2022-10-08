57d2acf2626975952249258ecc14a6b653616a5a
Matthew <perez_johnmatthewleonard@plpasig.edu.ph>
2022-10-08 21:16:41 +0800

    migration update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            // $table->string('name');
             $table->string('library_id_number');
-            $table->integer('student_info_id')->nullable();
+            $table->integer('student_info_id');
             $table->integer('compiled_backtrack_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
