9e1f57069a5852d6a2184346502e41e5eac1c731
Matthew <perez_johnmatthewleonard@plpasig.edu.ph>
2022-11-13 16:08:23 +0800

    role redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,21 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+    public function redirectTo() {
+        $role = Auth::user()->is_librarian;
+        switch ($role) {
+          case 1:
+            return '/overview';
+            break;
+          case 0:
+            return '/';
+            break;
+          default:
+            return '/';
+          break;
+        }
+      }
 
     /**
      * Create a new controller instance.
