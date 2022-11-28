8ad7222112a5a65c9c0bafeb9f1c96ef304d9274
Christian Kent <ckmagana07@gmail.com>
2022-11-28 19:23:50 +0800

    add change pass form

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,12 +17,70 @@
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 
-    {{-- <style>
-            .nav-link{
+     <style>
+            /* .nav-link{
             background-color: #4c9922;
             border-radius: 25px;
-        }
-        </style> --}}
+        } */
+        body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+/* Button used to open the contact form - fixed at the bottom of the page */
+
+/* The popup form - hidden by default */
+.form-popup {
+  display: none;
+  position: fixed;
+  bottom: 0;
+  right: 15px;
+  border: 3px solid #f1f1f1;
+  z-index: 9;
+}
+
+/* Add styles to the form container */
+.form-container {
+  max-width: 300px;
+  padding: 10px;
+  background-color: white;
+}
+
+/* Full-width input fields */
+.form-container input[type=text], .form-container input[type=password] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+}
+
+/* When the inputs get focus, do something */
+.form-container input[type=text]:focus, .form-container input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+
+/* Set a style for the submit/login button */
+.form-container .btn {
+  background-color: #04AA6D;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover {
+  opacity: 1;
+}
+        </style> 
 </head>
 
 <body style="background-image: linear-gradient(to left, green,#fde8ec);">
@@ -97,7 +155,8 @@
                             <li><p class="dropdown-item">
                                 <b>{{ $name }}</b>
                             </p></li>
-                            <li><p><a class="text-decoration-none dropdown-item" href="">Change Password</a></p></li>
+
+                            <li><p><a class="text-decoration-none dropdown-item" onclick="openForm()">Change Password</a></p></li>
                             
                             {{-- @endforeach --}}
                             {{-- <li><a class="dropdown-item"
@@ -112,10 +171,20 @@
                                 </form>
                             </li>
                         </ul>
-
-
-
-
+                        <div class="form-popup" id="myForm">
+                            <form action="/action_page.php" class="form-container">
+                              <h1>Change Password</h1>
+                          
+                              <label for="old-psw"><b>Password</b></label>
+                              <input type="password" placeholder="Enter New Password" name="old-psw" required>
+                          
+                              <label for="new-psw"><b>Password</b></label>
+                              <input type="password" placeholder="Re-enter Password" name="new-psw" required>
+                          
+                              <button type="submit" class="btn">Submit</button>
+                              <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+                            </form>
+                          </div>
 
 
                     </div>
@@ -280,6 +349,13 @@
     <script>
         function showhistory() {
           document.getElementById("history").innerHTML;
+        }
+        function openForm() {
+        document.getElementById("myForm").style.display = "block";
+        }
+
+        function closeForm() {
+        document.getElementById("myForm").style.display = "none";
         }
     </script>
 </body>
