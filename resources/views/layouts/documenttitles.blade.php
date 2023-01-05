04c4ed9215e1796c99029031a35ea093b3355d01
Matthew <perez_johnmatthewleonard@plpasig.edu.ph>
2023-01-05 23:22:33 +0800

    print updato

--- resources/views/layouts/documenttitles.blade.php
+++ resources/views/layouts/documenttitles.blade.php
@@ -1,45 +1,54 @@
 <div class="clean-blog-post" style="padding:20px; margin: 10px; border-style: inset;">
     <div class="row">
         @if (isset($document_studies))
-        @foreach ($document_studies as $document_study)
-            {{-- <div class="col-lg-5"><embed src="<?php //echo $links
-            ?>" width="100%" height="100%"
+            @foreach ($document_studies as $document_study)
+                {{-- <div class="col-lg-5"><embed src="<?php //echo $links
+                ?>" width="100%" height="100%"
                     style="border-style: solid;" /></div> --}}
-            <div class="col-lg-7">
-                <ul>
-                    <li><h3 style="color:black;"><a class="text-decoration-none text-dark" href="{{ route("viewpage",$document_study->document_id) }}">{{ $document_study->title }}</a></h3>
-                        <div class="info" style="color:black;"><b>(<span class="text-dark">{{ $document_study->college }} :
-                        {{ $document_study->document_type }}&nbsp;- {{ $document_study->document_number }})</b></span>
-                        </div>
-                        <div class="info text-dark">
-                        @if ($document_study->availability == "Available")
-                            <b class=" text-success">{{ $document_study->availability }}</b>
-                        @else
-                            <b class=" text-danger">{{ $document_study->availability }}</b>
-                        @endif
-                         </div>
-                        {{-- <div class="info" style="color:black;"><b>{{ $document_study->availability }}</b>
+                <div class="col-lg-7">
+                    <ul>
+                        <li>
+                            <h3 style="color:black;"><a class="text-decoration-none text-dark"
+                                    href="{{ route('viewpage', $document_study->document_id) }}">{{ $document_study->title }}</a>
+                            </h3>
+                            <div class="info" style="color:black;"><b>(<span
+                                        class="text-dark">{{ $document_study->college }} :
+                                        {{ $document_study->document_type }}&nbsp;-
+                                        {{ $document_study->document_number }})</b></span>
+                            </div>
+                            <div class="info text-dark">
+                                @if ($document_study->availability == 'Available')
+                                    <b class=" text-success">{{ $document_study->availability }}</b>
+                                @else
+                                    <b class=" text-danger">{{ $document_study->availability }}</b>
+                                @endif
+                            </div>
+                            {{-- <div class="info" style="color:black;"><b>{{ $document_study->availability }}</b>
                             </div> --}}
-                        <div class="info" style="color:black;"><span class="text-dark"><h6>TAGS: </h6>
-                            <div class="d-inline bg-success text-white rounded-pill">{{ $document_study->tag1 }}
-                            </div>&nbsp;|&nbsp;<div class="d-inline bg-success text-white rounded-pill">
-                                {{ $document_study->tag2 }}</div>&nbsp;|&nbsp;<div
-                                class="d-inline bg-success text-white rounded-pill"> {{ $document_study->tag3 }}
-                            </div>&nbsp;|&nbsp;<div class="d-inline bg-success text-white rounded-pill">
-                                {{ $document_study->tag4 }}</div>
-                        </span></div>
-                    </li>
+                            <div class="info tags-list" style="color:black;" id="tags">
+                                <span class="text-dark">
+                                    <h6>TAGS: </h6>
+                                    <div class="d-inline bg-success text-white rounded-pill">
+                                        {{ $document_study->tag1 }}
+                                    </div>&nbsp;|&nbsp;<div class="d-inline bg-success text-white rounded-pill">
+                                        {{ $document_study->tag2 }}</div>&nbsp;|&nbsp;<div
+                                        class="d-inline bg-success text-white rounded-pill"> {{ $document_study->tag3 }}
+                                    </div>&nbsp;|&nbsp;<div class="d-inline bg-success text-white rounded-pill">
+                                        {{ $document_study->tag4 }}</div>
+                                </span>
+                            </div>
+                        </li>
 
-                </ul>
+                    </ul>
 
-                <br>
-            </div>
-        @endforeach
+                    <br>
+                </div>
+            @endforeach
         @endif
         @if ($document_studies == null)
-        <div class="alert alert-info" role="alert">
-            Sorry, no documents found.
-        </div>
+            <div class="alert alert-info" role="alert">
+                Sorry, no documents found.
+            </div>
         @endif
     </div>
 </div>
