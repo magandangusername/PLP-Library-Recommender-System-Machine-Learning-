fd29344cdab658f568584e22c1be9624c79bba1a
Matthew <perez_johnmatthewleonard@plpasig.edu.ph>
2023-01-07 22:11:50 +0800

    capitalization fix

--- resources/views/SessionViews/accountancy.blade.php
+++ resources/views/SessionViews/accountancy.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 @section('content')
-        
+
         <section class="clean-block clean-blog-list dark" style="background-image: linear-gradient(to left, green,#fde8ec);">
             <div class="container d-flex justify-content-center">
-                <img class="imghover" src="{{ asset('img/course/accountancy.jfif') }}" alt="Accountancy Logo"
+                <img class="imghover" src="{{ asset('img/course/Accountancy.jfif') }}" alt="Accountancy Logo"
                     style="height:300px; width:300px; border-style: inset;">
             </div>
             <b>
